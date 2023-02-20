f2ea1e2f09a1539f6c341818f9bfe2c93780231a
iw-Ting <Ting@isweety.com.tw>
2023-02-20 17:37:19 +0800

    test

--- Assets/Script/Test/TestOne.cs
+++ Assets/Script/Test/TestOne.cs
@@ -12,37 +12,69 @@ public class TestOne : MonoBehaviour
     public string CallString = null;
 
     public TestTwo two;
+
+    public HashSet<string> a = new HashSet<string>();
+    public List<string> b = new List<string>();
+
     void Awake()
     {
+        a.Add("一");
+        a.Add("二");
+        a.Add("三");
+        a.Add("四");
+        a.Add("五");
+
+        b.Add("一");
+        b.Add("二");
+        b.Add("三");
+        b.Add("二");
+        b.Add("一");
+
+        foreach(string str in a){
+
+            Debug.Log("A==>"+str);
+        }
+        foreach(string str in b){
+
+            Debug.Log("B==>"+str);
+        }
+
+
         // Debug.Log("開始執行");
         // StartCoroutine(two.Init(Atest));
+
         // StartCoroutine(WaitCallBack());
-        
+        //StartCoroutine(WaitCallBack2());
+
+
 
     }
-    void Start()
-    {
 
-    }
 
-    private IEnumerator WaitCallBackValue(){
 
-        yield return new WaitForSeconds(3);
-        
-    }
 
 
     private IEnumerator WaitCallBack()
     {
-        Debug.Log("wait等待回傳參數" + CallString);
-        yield return CallString;
-        Debug.Log("callstring已收到回傳值" + CallString);
-
+        Debug.Log("等待回傳參數" + CallString);//2
+        yield return new WaitUntil(() => CallString != "");
+        Debug.Log("已收到回傳值" + CallString);//4
+        StopAllCoroutines();
     }
+
+    // private IEnumerator WaitCallBack2()
+    // {
+    //     Debug.Log("2等待回傳參數" + CallString);//2
+    //     yield return new WaitUntil(() => CallString == "失敗");
+    //     Debug.Log("2已收到回傳值" + CallString);//4
+    //     StopAllCoroutines();
+    // }
+
+
     private void Atest(string _str)
     {
         CallString = _str;
-        Debug.Log("我是腳本A的Atest");
+        Debug.Log("我是腳本A的Atest");//3
 
     }
 
--- Assets/Script/Test/TestTwo.cs
+++ Assets/Script/Test/TestTwo.cs
@@ -11,16 +11,16 @@ public class TestTwo : MonoBehaviour
 
     public IEnumerator Init( UnityAction<string> _aa)
     {
-    
+        Debug.Log("開始執行init");//1
         yield return new WaitForSeconds(3);
-        _aa("回傳參數給你成功");
+        _aa("成功了 we did it");
 
     }
 
-    public IEnumerator aaa(){
-        Debug.Log("成功執行a");
-        yield return new WaitForSeconds(1);
-    }
+    // public IEnumerator aaa(){
+    //     Debug.Log("成功執行a");
+    //     yield return new WaitForSeconds(1);
+    // }
 
 
 }
